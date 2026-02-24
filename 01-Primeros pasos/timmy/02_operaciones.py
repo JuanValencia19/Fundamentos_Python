2a31baaf908b219293efdbc107e32e4fd9942b91
santiagorf23 <santiraflo2002@gmail.com>
2026-02-23 19:21:53 -0500

    Ejercicio 1.2 (Timmy)

---
+++ 01-Primeros pasos/timmy/02_operaciones.py
@@ -0,0 +1,22 @@
+'''
+## 📘 Ejercicio 2 – Operaciones Básicas
+
+Declarar:
+a = 15  
+b = 4  
+
+Imprimir:
+- Suma
+- Resta
+- Multiplicación
+- División
+'''
+
+num1 = 15
+num2 = 4
+
+print(f"\nOperaciones Aritmeticas")
+print(f"Suma: 15 + 4 = {num1 + num2}")
+print(f"Resta: 15 - 4 =  {num1 - num2}")
+print(f"Multiplicacion: 15 * 4 = {num1 * num2}")
+print(f"Division: 15 / 4 = {num1 / num2}")
