caae919c61911fc17f4c42536352c00f75fdf6f5
juandev <juan.valencia10@utp.edu.co>
2026-02-23 21:36:54 -0500

    6 ejercicio de primeros pasos

---
+++ 01-Primeros pasos/juanjo/06-entrada-numerica.py
@@ -0,0 +1,13 @@
+# 🟢 Ejercicio 6 — Entrada Numérica y Conversión
+
+# Pide al usuario:
+
+# - Un número entero
+
+# Convierte el dato correctamente y calcula su doble.
+
+# Imprime el resultado.
+
+numero = int(input("Pon un numero que quieras calcular el doble: "))
+doble = numero * 2
+print(f"El doble de su numero es: {doble}")
