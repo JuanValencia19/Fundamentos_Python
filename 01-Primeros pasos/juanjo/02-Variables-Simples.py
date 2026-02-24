78494818cf79778ce6f2f1846feff158cff9e9c4
juandev <juan.valencia10@utp.edu.co>
2026-02-23 21:07:41 -0500

    Cambio de ejercicios primeros pasos y ejercicio 3 realizado

---
+++ 01-Primeros pasos/juanjo/02-Variables-Simples.py
@@ -0,0 +1,17 @@
+#🟢 Ejercicio 2 — Variables Simples
+
+# Declara tres variables:
+
+# - nombre (string)
+# - edad (int)
+# - altura (float)
+
+# Imprime todas en una sola línea usando print().
+
+nombre = "Juan Jose"
+edad = 22
+altura = 1.75
+
+print(f"Mi nombre es: {nombre} y tengo {edad} y mido {altura}")
+
+
---
+++ 01-Primeros pasos/juanjo/03-Operaciones-Matematicas.py
@@ -0,0 +1,23 @@
+# 🟢 Ejercicio 3 — Operaciones Matemáticas Básicas
+
+# Declara:
+
+# a = 20  
+# b = 5  
+
+# Calcula e imprime:
+
+# - Suma
+# - Resta
+# - Multiplicación
+# - División
+
+a = 20
+b = 5
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+print(f"La suma es: {suma}. La resta es: {resta}. La multiplicacion: {multiplicacion}. Division: {division}")
