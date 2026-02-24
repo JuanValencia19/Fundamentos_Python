73d31b24687a541c4729bf6983f39258d95a0daa
santiagorf23 <santiraflo2002@gmail.com>
2026-02-23 19:12:43 -0500

    Presentacion (Timmy)

---
+++ 01-Primeros pasos/timmy/01_presentacion.py
@@ -0,0 +1,26 @@
+'''
+📝 MI COMPROMISO OFICIAL
+Fecha de inicio: [Febrero 23 del 2026]
+Meta: Convertirme en the best programador y realizar todos los proyectos con mi gordito
+'''
+
+'''
+📘 Ejercicio 1 – Presentación Personal
+
+Crear un programa que:
+- Guarde tu nombre en una variable
+- Guarde tu edad en otra variable
+- Imprima un mensaje de presentación
+'''
+
+name = "Santiago"
+nickname = "Timmy"
+age = 23
+city = "Pereira"
+occupation = "Ingeniero en Desarrollo de Software"
+occupation_soon = "FullStack"
+
+print("="*60)
+print("🔥 FIRMA DIGITAL:")
+print(f"Mi nombre es {name} pero todo mundo me conoce como {nickname}, vivo en la ciudad de {city}, tengo {age} de edad, mi profesion es {occupation} pero muy pronto sere {occupation_soon}")
+print("="*60)
