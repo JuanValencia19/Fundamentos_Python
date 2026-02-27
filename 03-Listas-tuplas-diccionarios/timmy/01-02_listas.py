52aae71c1b4df4ecf91d41f20477378d5386952f
santiagorf23 <santiraflo2002@gmail.com>
2026-02-26 19:14:58 -0500

    Ejercicio 1 y 2 (Listas)

---
+++ 03-Listas-tuplas-diccionarios/timmy/01-02_listas.py
@@ -0,0 +1,23 @@
+'''
+## 1️⃣ Crear y mostrar lista
+Crea una lista con 5 equipos de fútbol.
+Imprime la lista completa.
+'''
+import os
+os.system("cls")
+
+equipos_futbol = ["Manchester United", "Real Madrid", "Liverpool", "Arsenal", "Deportivo Pereira"]
+print(equipos_futbol)
+
+'''
+## 2️⃣ Acceder a elementos
+Imprime:
+- El primer equipo
+- El último equipo
+'''
+
+equipos_futbol = ["Manchester United", "Real Madrid", "Liverpool", "Arsenal", "Deportivo Pereira"]
+
+print(equipos_futbol)
+print(f"Primer equipo de la lista: {equipos_futbol[0]}")
+print(f"Ultimo equipo de la lista: {equipos_futbol[-1]}")
