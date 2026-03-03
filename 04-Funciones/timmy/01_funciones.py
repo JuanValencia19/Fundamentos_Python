5d2704330efed031cc23f174f8e4088697cd88f5
santiagorf23 <santiraflo2002@gmail.com>
2026-03-02 20:25:58 -0500

    Ejercicios 1 y 2 (Funciones)

---
+++ 04-Funciones/timmy/01_funciones.py
@@ -0,0 +1,14 @@
+'''
+## 1️⃣ Función de saludo
+Crea una función llamada `saludar()` que imprima:
+"Bienvenido al sistema de análisis deportivo"
+
+Llama la función.
+'''
+import os
+os.system("cls")
+
+def saludar():
+    print("Bienvenido al sistema de analisis deportivo")
+    
+saludar()
