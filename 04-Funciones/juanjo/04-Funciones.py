eeec25bf70b6f8f483c9eb54697c6edcbca60341
juandev <juan.valencia10@utp.edu.co>
2026-03-04 21:43:33 -0500

    Realice ejercicios 4 y 5 de funciones

---
+++ 04-Funciones/juanjo/04-Funciones.py
@@ -0,0 +1,11 @@
+# 4️⃣ Función para calcular cuota
+# Crea una función `probabilidad(cuota)` que retorne:
+
+# 1 / cuota
+
+# Muestra el resultado.
+
+def calcular_couta(couta):
+    return 1 / couta
+
+print(calcular_couta(1.80))
---
+++ 04-Funciones/juanjo/06-Funciones.py
@@ -0,0 +1,8 @@
+# 6️⃣ Ganancia de apuesta
+# Crea una función:
+
+# calcular_ganancia(monto, cuota, gano)
+
+# Si ganó → retorna monto * cuota
+# Si perdió → retorna 0
+
