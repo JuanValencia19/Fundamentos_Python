eaabdc7359871477d2960cf4ac012a54c91e63e1
juandev <juan.valencia10@utp.edu.co>
2026-03-04 21:49:24 -0500

    Ejercicio 6 funciones

--- 04-Funciones/juanjo/06-Funciones.py
+++ 04-Funciones/juanjo/06-Funciones.py
@@ -6,3 +6,10 @@
 # Si ganó → retorna monto * cuota
 # Si perdió → retorna 0
 
+def  calcular_ganancia(monto, couta, gano):
+    if gano == True:
+        return monto * couta
+    else: 
+        return 0
+print(calcular_ganancia(1400, 2.30, True))
+print(calcular_ganancia(1400, 2.30, False))
