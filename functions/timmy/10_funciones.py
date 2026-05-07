0bcd41745da9057e60254972c15acf81c45c8e2d
santiagorf23 <santiraflo2002@gmail.com>
2026-05-06 21:02:34 -0500

    ejercicio 10 funciones

---
+++ functions/timmy/10_funciones.py
@@ -0,0 +1,13 @@
+'''
+## 🔟 Contador de resultados
+
+Crea una función que reciba una lista como:
+
+["local", "empate", "local", "visitante"]
+
+Y retorne un diccionario con el conteo.
+'''
+
+from modules.module import clear_execution
+
+clear_execution()
